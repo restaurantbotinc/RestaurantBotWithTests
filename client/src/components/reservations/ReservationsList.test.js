10c1e6cae1d5ac05a4326122d16d0721366a2ba9
Katharine Francis <katie-francis@hotmail.com>
2019-07-14 16:43:24 -0400

    Added ReservationList component test

---
+++ client/src/components/reservations/ReservationsList.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import ReservationList from "./ReservationsList";
+
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("ReservationList", () => {
+  it("should render a <div />", () => {
+    const wrapper = shallow(<ReservationList className="App" />);
+    // const classWrapper = wrapper.hasClass("App");
+    // return classWrapper;
+    expect(wrapper.find("div").length).toEqual(1);
+  });
+});

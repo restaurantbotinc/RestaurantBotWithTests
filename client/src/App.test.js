acd707f9054e46e8d22ada219e4c4652f68ac2b7
Amy Kim <amesk3@gmail.com>
2019-07-15 15:38:34 -0400

    added react enzyme tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ReservationsList from "./components/reservations/ReservationsList";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("should reder a <div/>", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("div").length).toEqual(1);
+});
+
+it("should reder a <div/>", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("ReservationsList").length).toEqual(1);
 });
